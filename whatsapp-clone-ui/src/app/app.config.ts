41ac177abf1a74a8d5b2e69808a0419a97c13205
LN REDDY <lnreddyreddy405@gmail.com>
2025-07-05 20:19:01 +0530

    Adding app config details..

--- whatsapp-clone-ui/src/app/app.config.ts
+++ whatsapp-clone-ui/src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { KeycloakService } from './utils/keycloak/keycloak';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAppInitializer(()=>{
+      const initFn=((key: KeycloakService)=>{
+        return ()=>key.init();
+      }) (inject(KeycloakService));
+
+      return initFn();
+    })
+
   ]
 };
